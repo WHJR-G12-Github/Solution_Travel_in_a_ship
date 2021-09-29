3701e05ca89dacfba8256223cd3e608c0a656daf
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-09-29 11:03:29 +0530

    Update solution_ship.py

--- solution_ship.py
+++ solution_ship.py
@@ -8,12 +8,15 @@ width=500
 height=400
 screen = pygame.display.set_mode((width,height))
 
-#load images
+# Loading images
+# Creating an empty dictionary 'images'
 images={}
+# Loading the background image 'sea.png' into the 'images' dictionary under the name 'bg'. Now, 'images["bg"]' refers to the background image
 images["bg"] = pygame.image.load("sea.png").convert_alpha()
+# Loading the ship image 'ship.png' into the 'images' dictionary under the name 'ship'. Now, 'images["ship"]' refers to the ship image
 images["ship"] = pygame.image.load("ship.png").convert_alpha()
 
-#variable to hold x-axis value of background image
+# Creating a variable 'bgx' to hold x-axis value of background image and initialize it to zero (Eg. a=0 . Here, variable 'a' is created & initialized to zero)
 bgx=0
 
 while True:    
@@ -23,16 +26,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    #decrement bgx to move the background
+    # Decrementing the value of 'bgx' by 5 to move the background 
     bgx=bgx-5
     
-    # Add code to reset bgx to 0 after bgx becomes less then -1000
+    # Code to reset bgx to 0 after bgx becomes less then -1000
+    # Checking if 'bgx' is less than -1000 
     if   bgx < -1000  :
+        # Resetting the value of 'bgx' to zero
         bgx=0
     
-    #display the background and ship image
+    # Displaying the ship image at the coordinates '[100,150]'
+    screen.blit(images["ship"],[100,150]) 
+    # Displaying the background image at the coordinates '[bgx,0]'
     screen.blit(images["bg"],[bgx,0])
-    screen.blit(images["ship"],[100,150])  
+     
     
    
     pygame.display.update()
